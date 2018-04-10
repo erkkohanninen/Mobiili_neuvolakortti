fb781cd585c1210fc543852f28dd8d266aace701
Juho Kettunen <jekettu@gmail.com>
2018-04-10 15:20:13 +0300

    Aloitussivu

---
+++ app/src/main/java/mobiili/neuvolakortti/ChildAdapter.java
@@ -0,0 +1,90 @@
+package mobiili.neuvolakortti;
+
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/*
+ * {@link AndroidFlavorAdapter} is an {@link ArrayAdapter} that can provide the layout for each list
+ * based on a data source, which is a list of {@link AndroidFlavor} objects.
+ * */
+public class ChildAdapter extends RecyclerView.Adapter<ChildAdapter.MyViewHolder> {
+    public List<Child> myValues;
+    public ChildAdapter (List<Child> myValues){
+        this.myValues= myValues;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View listItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.child_list_item, parent, false);
+        return new MyViewHolder(listItem);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.currentChild = myValues.get(position);
+        holder.nameTextView.setText(holder.currentChild.getName());
+        holder.ageTextView.setText(calcAge(holder.currentChild.getDateOfBirth()));
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return myValues.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView nameTextView;
+        private TextView ageTextView;
+        public View view;
+        public Child currentChild;
+
+        public MyViewHolder(final View itemView) {
+            super(itemView);
+            view = itemView;
+            //Click listener for profile
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    //Intent intent = new Intent(v.getContext(), AddChildActivity.class);
+                    //anv.getContext().startActivity(intent);
+                }
+            });
+            nameTextView = itemView.findViewById(R.id.tv_child_name);
+            ageTextView = itemView.findViewById(R.id.tv_child_age);
+        }
+    }
+
+
+    // Calculate age from todays date and birthdate
+    public static String calcAge(String birthDate) {
+        SimpleDateFormat fullFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date dateOfBirth = null;
+        Date dateOfToday = new Date();
+
+        try {
+            dateOfBirth = fullFormat.parse(birthDate);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        long ageMs = dateOfToday.getTime() - dateOfBirth.getTime();
+
+        long ageYear = ageMs/31556952000L;
+        long ageMonth = (ageMs % 31556952000L) / 2629746000L;
+        long ageDays = ((ageMs % 31556952000L) % 2629746000L) / 86400000;
+
+        Log.d("Year", "calcAge: " + ageYear +"years, " + ageMonth +"months, " +ageDays + "days");
+
+        String age = ageYear +"v, " + ageMonth +"kk, " +ageDays + "pv";
+
+        return age;
+    }
+}
--- app/src/main/java/mobiili/neuvolakortti/MainActivity.java
+++ app/src/main/java/mobiili/neuvolakortti/MainActivity.java
@@ -3,8 +3,11 @@ package mobiili.neuvolakortti;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.util.Log;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // RecyclerView
+        ChildAdapter mAdapter;
+        RecyclerView mChildrenList;
+
+        //Database
+        DbAdapter db;
+        db = new DbAdapter(this);
+        db.open();
+        List<Child> children = db.getAllChildren(); //All childeren to list
+
+        // RecyclerView
+        mChildrenList = findViewById(R.id.rv_children);
+        mAdapter = new ChildAdapter(children);
+        mChildrenList.setAdapter(mAdapter);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        mChildrenList.setLayoutManager(llm);
+
     }
 
+    //Floating Action Button
     void goToAddChild(View view){
         Intent intent = new Intent(this, AddChildActivity.class);
         startActivity(intent);
