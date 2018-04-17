219b56055e8189dadd7de2c471dd404718cdca74
Erkko Hänninen <ossi.oppilas@oamk.fi>
2018-04-17 16:46:00 +0300

    Lisätty MPAndroidChart dependencyt build.gradleihin. Testailtu LineChartin näyttämistä fragmenteissa.

--- app/src/main/java/mobiili/neuvolakortti/HeightFragment.java
+++ app/src/main/java/mobiili/neuvolakortti/HeightFragment.java
@@ -7,10 +7,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+
 public class HeightFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_height,container,false);
+
+        View v = inflater.inflate(R.layout.fragment_height,container,false);
+        LineChart chart = (LineChart) v.findViewById(R.id.height_chart);
+
+        ArrayList<Entry> entries = new ArrayList<>();
+
+        entries.add(new Entry(1,2));
+        entries.add(new Entry(2,3));
+
+        LineDataSet dataSet = new LineDataSet(entries, "Pituus");
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+
+        chart.invalidate();
+
+        return v;
     }
 }
